e2e7bc7a74ae33e1d510f8bfae136d191e467b75
Erika Tsuchiya <tsuchiya.erika@synergy101.jp>
2019-10-20 18:09:08 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, {FC, useState, useEffect} from 'react'
+import {Button, Card, Statistic} from 'semantic-ui-react';
+
+import './App.css';
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+
+  const answer = (count:number) => {
+    if (count === 0) {
+      return '-';
+    } else if (count % 15 === 0) {
+      return 'FizzBuzz';
+    } else if (count % 3 === 0) {
+      return 'Fizz';
+    } else if (count % 5 === 0) {
+      return 'Buzz';
+    } else {
+      return '-';
+    }
+  }
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  useEffect(() => {
+    console.log('Please click button!')
+  }, [])
+
+  return (
+    <div className="container">
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>count</Statistic.Label>
+          <Statistic.Value>{count}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <div className="ui three buttons">
+            <Button color="red" onClick={decrement}>-1</Button>
+            <Button color="yellow" onClick={reset}>reset</Button>
+            <Button color="blue" onClick={increment}>+1</Button>
+          </div>
+          <Card.Header className="center">FizzBuzz?</Card.Header>
+          <Card.Description className="center">{answer(count)}</Card.Description>
+        </Card.Content>
+      </Card>
+    </div>
+  )
+}
+
+export default App;
